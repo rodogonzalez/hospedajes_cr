461e308ee950df7a197ac8df3e17ac66300935b2
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2022-11-29 11:53:11 -0600

    ssss

--- resources/views/front/main.blade.php
+++ resources/views/front/main.blade.php
@@ -71,6 +71,30 @@
       icon: iconBase + "info-i_maps.png",
     },
   };
+
+
+
+
+ const svgMarker = {
+            path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+            fillColor: "blue",
+            fillOpacity: 0.6,
+            strokeWeight: 0,
+            rotation: 0,
+            scale: 2,
+            //anchor: new google.maps.Point(15, 30),
+          };
+
+  const svgMarkerHosting = {
+    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+    fillColor: "black",
+    fillOpacity: 0.9,
+    strokeWeight: 0,
+    rotation: 0,
+    scale: 2,    
+  };
+
+
  map_points= [];
 
 
@@ -140,12 +164,14 @@ function pull_country_parts(country_slug){
           options_content= options_content + "<option value='" +  item.slug + "'>" +  item.slug + "</option>";
           
 
+          
+
           // The marker, positioned at Uluru
           point_marker = new google.maps.Marker({
             position: uluru,
             title: item.slug,
             map: map,
-            //icon: icons['library'].icon,
+            icon: svgMarker,
 
           });
 
@@ -230,7 +256,8 @@ function pull_country_parts_destinations_commerces(country_slug,section,destinat
             position: uluru,
             title: item.name,
             map: map,
-            icon: icons['parking'].icon,
+            //icon: icons['parking'].icon,
+            icon:svgMarkerHosting
           });
 
           let description =  "<hr><b>Descripcion:</b><div>" + item.description + "</div>" ;
