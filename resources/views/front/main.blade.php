7ebe19ae59dadb4d72f340bdbdd5bdbe38d9120f
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2023-02-23 15:54:32 -0600

    add ads

--- resources/views/front/main.blade.php
+++ resources/views/front/main.blade.php
@@ -293,6 +293,21 @@ window.onload = initMap;
 
 </script>
   @yield('content')
+
+
+
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3258144106657369"
+     data-ad-slot="8812111908"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+	</script> 
+
+
   </div>
   
   
