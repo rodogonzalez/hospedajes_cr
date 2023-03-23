c32970bcba85c33a6099ea2c0ad12c0bc734762c
rodolfo <rodogonzalez@msn.com>
2023-03-23 10:45:16 -0600

    fix location 3

--- app/Http/Controllers/Admin/AbstractLocationFields.php
+++ app/Http/Controllers/Admin/AbstractLocationFields.php
@@ -201,7 +201,7 @@ function pull_country_parts(country_slug){
                 // The marker, positioned at Uluru
                 marker = new google.maps.Marker({
                         draggable: true,
-                        position: {lat:  parseFloat(document.getElementById("position_lat").value) ,lng:  parseFloat (document.getElementById("position_lng").value) },
+                        position: {lat:document.getElementById("position_lat").value ,lng:document.getElementById("position_lng").value},
                         title: "Aqui",
                         //label: item.name,
                         map: map,
